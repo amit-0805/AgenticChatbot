f82c5a45c581740e14d4e0c07432aeb099e4a1d1
Amit <2003.a.amit@gmail.com>
2025-09-07 15:24:00 +0530

    Chatbot implemented with tools

--- src/langgraph/UI/streamlitui/display_result.py
+++ src/langgraph/UI/streamlitui/display_result.py
@@ -23,3 +23,19 @@ class DisplayResultStreamlit:
                             st.write(user_message)
                         with st.chat_message("assistant"):
                             st.write(value["messages"].content)
+
+        elif usecase == "Chatbot with Web":
+             initital_state = {"messages": [user_message]}
+             res = graph.invoke(initital_state)
+             for message in res["messages"]:
+                  if type(message) == HumanMessage:
+                       with st.chat_message("user"):
+                            st.write(message.content)
+                  elif type(message) == AIMessage and message.content:
+                        with st.chat_message("assistant"):
+                            st.write(message.content)
+                  elif type(message) == ToolMessage:
+                        with st.chat_message("ai"):
+                            st.write("Tool call start")
+                            st.write(message.content)
+                            st.write("Tool call end")
--- src/langgraph/graph/graph_builder.py
+++ src/langgraph/graph/graph_builder.py
@@ -1,6 +1,9 @@
 from langgraph.graph import StateGraph, START, END
 from src.langgraph.state.state import State
 from src.langgraph.nodes.basic_chatbot_node import BasicChatbotNode
+from src.langgraph.tools.search_tool import create_tool_node, get_tools
+from langgraph.prebuilt import tools_condition
+from src.langgraph.nodes.chatbot_with_tool import ChatbotWithTool
 
 class GraphBuilder:
     def __init__(self, model):
@@ -15,8 +18,27 @@ class GraphBuilder:
         self.graph_builder.add_edge(START, "chatbot")
         self.graph_builder.add_edge("chatbot", END)
 
+    def chatbot_with_tools_build_graph(self):
+        ## Define tool and toolNode
+        tools = get_tools()
+        tool_node = create_tool_node(tools)
+        llm = self.llm
+
+        obj_chatbot_with_tool = ChatbotWithTool(llm)
+        chatbot_node = obj_chatbot_with_tool.create_chatbot(tools)
+
+        #nodes
+        self.graph_builder.add_node("chatbot", chatbot_node)
+        self.graph_builder.add_node("tools", tool_node)
+        self.graph_builder.add_edge(START, "chatbot")
+        self.graph_builder.add_conditional_edges("chatbot",tools_condition)
+        self.graph_builder.add_edge("tools", "chatbot")
+        self.graph_builder.add_edge("chatbot", END)
+
     def setup_graph(self, usecase: str):
         if usecase == "Basic Chatbot":
             self.basic_chatbot_build_graph()
+        if usecase == "Chatbot with Web":
+            self.chatbot_with_tools_build_graph()
 
         return self.graph_builder.compile()
