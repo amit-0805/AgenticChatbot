e73f05b4b24edfedf91ffca2708903bb168a75a0
Amit <2003.a.amit@gmail.com>
2025-09-07 14:42:28 +0530

    Final Chatbot

---
+++ src/langgraph/state/state.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel, Field
+from typing import TypedDict, Annotated, List
+from langgraph.graph.message import add_messages
+
+class State(TypedDict):
+    messages: Annotated[List, add_messages]
